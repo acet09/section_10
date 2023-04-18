0ab6bf4b6e3ec3e429c5de926b77c1349d840636
acet09 <ysjungman@gmail.com>
2023-04-19 07:46:04 +0900

    133_context_활용

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
     <AuthContext.Provider
       value={{
         isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
       }}
     >
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
